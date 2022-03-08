ff177161152139a4efa4597b949dcf6913d64f6e
Uriel2B <101163340+UrielUpsin@users.noreply.github.com>
2022-03-07 22:03:40 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+ const http = require("http");
+ const express = require("express");
+ const app = express();
+
+ // Codificar request / result
+
+ app.get('/',(req,res)=> {
+
+    res.send("Iniciamos Servidor")
+ })
+
+ const puerto = 3000 ;
+ app.listen(puerto,()=>{
+
+    console.log(" Escuchando");
+ });
